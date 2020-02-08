a9a384e82e266fedefe7d5a82699e382e45a3e5d
Nam Le <nam.leduc02@gmail.com>
2020-02-08 22:19:23 +0100

    add update data hook

--- src/components/Funnel/index.js
+++ src/components/Funnel/index.js
@@ -1,17 +1,17 @@
-import React, { useEffect, useRef } from 'react'
-import PropTypes from 'prop-types'
+import React, { useEffect, useRef } from "react";
+import PropTypes from "prop-types";
 
-import FunnelGraph from '../../lib/init'
+import FunnelGraph from "../../lib/init";
 
-const normalizeData = (data, key) => data.map(item => item[key])
+const normalizeData = (data, key) => data.map(item => item[key]);
 
-const renderPipeline = (config) => {
-  var graph = new FunnelGraph(config)
-  graph.draw()
-}
+const renderPipeline = config => {
+  var graph = new FunnelGraph(config);
+  graph.draw();
+};
 
 const Funnel = props => {
-  const funnelRef = useRef(null)
+  const funnelRef = useRef(null);
 
   useEffect(() => {
     const {
@@ -23,30 +23,63 @@ const Funnel = props => {
       colors,
       gradientDirection,
       responsive
-    } = props
+    } = props;
 
     renderPipeline({
       container: funnelRef.current,
       data: {
         colors,
-        labels: props.hasOwnProperty('labelKey') ? normalizeData(data, labelKey) : [],
-        values: props.hasOwnProperty('valueKey') ? normalizeData(data, valueKey) : []
+        labels: props.hasOwnProperty("labelKey")
+          ? normalizeData(data, labelKey)
+          : [],
+        values: props.hasOwnProperty("valueKey")
+          ? normalizeData(data, valueKey)
+          : []
       },
       displayPercent: props.displayPercent,
       gradientDirection,
       height,
       responsive,
       width
-    })
-    
-  }, [])
+    });
+  }, []);
 
-  return <div ref={funnelRef} />
-}
+  useEffect(() => {
+    const {
+      data,
+      labelKey,
+      valueKey,
+      height,
+      width,
+      colors,
+      gradientDirection,
+      responsive
+    } = props;
 
+    renderPipeline({
+      container: funnelRef.current,
+      data: {
+        colors,
+        labels: props.hasOwnProperty("labelKey")
+          ? normalizeData(data, labelKey)
+          : [],
+        values: props.hasOwnProperty("valueKey")
+          ? normalizeData(data, valueKey)
+          : []
+      },
+      displayPercent: props.displayPercent,
+      gradientDirection,
+      height,
+      responsive,
+      width
+    });
+  }, [props.data]);
+
+  return <div ref={funnelRef} />;
+};
 
 Funnel.propTypes = {
-  data:  PropTypes.array.isRequired,
+  data: PropTypes.array.isRequired,
   valueKey: PropTypes.string.isRequired,
   labelKey: PropTypes.string,
   height: PropTypes.number.isRequired,
@@ -54,5 +87,4 @@ Funnel.propTypes = {
   responsive: PropTypes.bool
 };
 
-export default React.memo(Funnel)
- 
+export default React.memo(Funnel);
